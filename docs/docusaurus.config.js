54fbbcab817a0e1f6b73ebd0ab6be6c16840336b
Craig Ingram <cinple.sln@gmail.com>
2025-05-29 17:15:58 -0400

    Updated docusarus version and other dependencies (#48)

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,7 +1,7 @@
 const isCI = !!process.env.CI;
 
-/** @type {import('@docusaurus/types').DocusaurusConfig} */
-module.exports = {
+/** @type {import('@docusaurus/types').Config} */
+const config = {
   title: 'Operator for Redis Cluster',
   url: isCI ? 'https://cin.github.io' : 'http://localhost:3001',
   baseUrl: '/operator-for-redis-cluster/',
@@ -11,63 +11,75 @@ module.exports = {
   trailingSlash: false,
   organizationName: 'cin',
   projectName: 'operator-for-redis-cluster',
-  themeConfig: {
-    docs: {
-      sidebar: {
-        hideable: true,
-      }
-    },
-    colorMode: {
-      defaultMode: 'dark',
-    },
-    navbar: {
-      hideOnScroll: false,
-      title: 'Operator for Redis Cluster',
-      logo: {
-        src: 'images/logo.svg',
-        srcDark: 'images/logo.svg',
+  
+  // GitHub pages deployment config
+  deploymentBranch: 'gh-pages',
+  
+  themeConfig: 
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      docs: {
+        sidebar: {
+          hideable: true,
+        }
       },
-      items: [
-        {
-          href: 'https://cin.github.io/operator-for-redis-cluster',
-          label: 'GitHub',
-          position: 'right',
+      colorMode: {
+        defaultMode: 'dark',
+      },
+      navbar: {
+        hideOnScroll: false,
+        title: 'Operator for Redis Cluster',
+        logo: {
+          src: 'images/logo.svg',
+          srcDark: 'images/logo.svg',
         },
-      ],
-    },
-    prism: {
-      defaultLanguage: 'go',
-      additionalLanguages: ['go'],
-    },
-    footer: {
-      style: 'dark',
-      links: [],
-      copyright: `Operator for Redis Cluster Documentation. Built with Docusaurus.`,
-    },
-  },
+        items: [
+          {
+            href: 'https://cin.github.io/operator-for-redis-cluster',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      prism: {
+        defaultLanguage: 'go',
+        additionalLanguages: ['go'],
+      },
+      footer: {
+        style: 'dark',
+        links: [],
+        copyright: `Operator for Redis Cluster Documentation. Built with Docusaurus.`,
+      },
+    }),
+  
   presets: [
     [
       '@docusaurus/preset-classic',
-      {
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
         docs: {
           sidebarCollapsible: true,
           showLastUpdateTime: true,
           routeBasePath: '/',
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          editUrl:
-            'https://cin.github.io/operator-for-redis-cluster',
+          editUrl: 'https://cin.github.io/operator-for-redis-cluster',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://cin.github.io/operator-for-redis-cluster',
+          editUrl: 'https://cin.github.io/operator-for-redis-cluster',
         },
-      },
+      }),
     ],
   ],
+  
   plugins: [
-    ['@docusaurus/plugin-client-redirects', { fromExtensions: ['html', 'md'] }],
-  ]
+    [
+      '@docusaurus/plugin-client-redirects', 
+      { 
+        fromExtensions: ['html', 'md'] 
+      }
+    ],
+  ],
 };
+
+module.exports = config;
